bffe939364e1b0513a3fdeda82d599bc40099e0e
dajebbar <dajebbar@gmx.fr>
2020-05-18 14:45:21 +0000

    Storing bullet in a Group and Firing bullet.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,10 +1,8 @@
-# Creating a Pygame Window and Responding to User Input
-# Setting the Background Color
-
 import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 
 class AlienInvasion:
@@ -27,6 +25,7 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -35,6 +34,7 @@ class AlienInvasion:
 
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
 
     def _check_events(self):
@@ -62,6 +62,9 @@ class AlienInvasion:
         elif event.key == pygame.K_q:
             sys.exit()
 
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+
     def _check_keyup_events(self, event):
         """Respond to key releases."""
 
@@ -71,6 +74,11 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
 
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group."""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
 
@@ -78,6 +86,9 @@ class AlienInvasion:
         self.screen.fill(self.settings.bg_color)
 
         self.ship.blitme()
+
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
