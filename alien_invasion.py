5e6431672e932d81be2e2e82d86037024ecdb637
dajebbar <dajebbar@gmx.fr>
2020-05-18 12:23:28 +0000

    Allowing continious mvmt to ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -28,6 +28,7 @@ class AlienInvasion:
         while True:
 
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
     def _check_events(self):
@@ -35,6 +36,16 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+            elif event.type == pygame.KEYDOWN:
+
+                if event.key == pygame.K_RIGHT:
+                    # Move the ship to the right.
+                    self.ship.moving_right = True
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
